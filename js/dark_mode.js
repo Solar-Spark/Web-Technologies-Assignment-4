fe3662bc3e1bb33c5135145134dc13f39bcf2cd1
perpavbek <121007004+perpavbek@users.noreply.github.com>
2024-10-14 22:53:58 +0500

    darkmode

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,19 +1,11 @@
 let darkmode = true;
-let darkMap = new Map().set('background-color', '#020307').set('text-color', '#FFFFFF').set('button-color', '#FFFFFF');
-let lightMap = new Map().set('background-color', '#E4E4E4').set('text-color', '#000000').set('button-color', '#141414');
 
 function setDarkMode(){
     darkmode = !darkmode;
     if(darkmode){
-        setStyles(darkMap);
+        document.body.classList.remove('light');
     }
     else{
-        setStyles(lightMap);
+        document.body.classList.add('light');
     }
-}
-
-function setStyles(map){
-    document.body.style.backgroundColor = map.get("background-color");
-    document.body.style.color = map.get("text-color");
-    document.getElementsByClassName("darkmode-btn")[0].style.backgroundColor = map.get("button-color");
 }
