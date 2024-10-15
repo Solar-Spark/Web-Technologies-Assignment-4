f02f5f35daf363ef499575561701744e2b009363
R0onen <erni100105@gmail.com>
2024-10-15 08:20:28 +0500

    added sorting of array numbers

--- js/script.js
+++ js/script.js
@@ -14,3 +14,24 @@ setInterval(function(){
     let date = new Date();
     document.getElementById("clock").innerHTML = date.toDateString() + ', ' + date.toLocaleTimeString();
     }, 1000);
+
+function sortNumbers(input, order = 'asc') {
+    // Convert the input string into an array of numbers
+    let numArray = input.split(/[\s,]+/).map(Number);
+    
+    // Sort the array based on the order parameter
+    numArray.sort(function(a, b) {
+        return order === 'asc' ? a - b : b - a;
+    });
+    
+    return numArray;
+}
+
+document.getElementById('sortButton').addEventListener('click', function() {
+    const userInput = document.getElementById('numberInput').value;
+    const sortOrder = document.getElementById('sortOrder').value;
+    
+    const sortedNumbers = sortNumbers(userInput, sortOrder);
+    document.getElementById('result').innerText = sortedNumbers.join(', ');
+});
+
